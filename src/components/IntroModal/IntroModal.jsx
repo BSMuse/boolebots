1b28513a587058024fb8264914753eff057bc03a
BSMUse <bsmuse@tutamail.com>
2023-07-31 09:43:51 -0600

    Modify winner modal and arena for properly functioning component

--- src/components/IntroModal/IntroModal.jsx
+++ src/components/IntroModal/IntroModal.jsx
@@ -1,1 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
+
+function IntroModal() {
+    useEffect(() => {
+
+    })
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    );
+}
