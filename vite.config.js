812c4f6dc60e601642644176090810b5db0d2e79
BSMUse <bsmuse@tutamail.com>
2023-08-09 15:19:27 -0600

    update config for proper image handling

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
+export default defineConfig({
+  build: {
+    outDir: 'build',
+    assetsDir: 'assets', // This specifies where your assets will be placed in the build output directory
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // Create an alias for your images directory
+      '@images': '/src/images',
     },
-    plugins: [react()],
-  };
+  },
 });
